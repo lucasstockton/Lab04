246146dbe25c12c6e64f0fa734157bcd3c9444a7
lucasstockton <35933381+lucasstockton@users.noreply.github.com>
2018-04-07 21:10:20 -0400

    First Branch

---
+++ MvcMovie/Models/MPAA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MPAA
+    {
+
+
+        public string MPRating { get; set; }
+
+        public List<SelectListItem> MPRatingList { get; } = new List<SelectListItem>
+        {
+        new SelectListItem {Text = "G", Value = "G"},
+        new SelectListItem {Text = "PG", Value = "PG"},
+        new SelectListItem {Text = "PG-13", Value = "PG-13"},
+        new SelectListItem {Text = "R", Value = "R"},
+        new SelectListItem {Text = "NC-17", Value = "NC-17"},
+        new SelectListItem {Text = "NR", Value = "NR"}
+        };
+
+
+    }
+}
+
+
+
+
+
+
+/*
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace FormsTagHelper.ViewModels
+{
+    public class CountryViewModel
+    {
+        public string Country { get; set; }
+
+        public List<SelectListItem> Countries { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "MX", Text = "Mexico" },
+            new SelectListItem { Value = "CA", Text = "Canada" },
+            new SelectListItem { Value = "US", Text = "USA"  },
+        };
+    }
+} */
